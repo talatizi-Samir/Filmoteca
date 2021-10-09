002a50d2bb4eaf78ac29e71f235fbfa19a270039
Talatizi Samir <talatizisamir@gmail.com>
2021-10-09 03:44:02 +0200

    Ejercicio 3. Añadiendo contenido a la actividad

--- app/src/main/java/es/ua/eps/filmoteca/AboutActivity.kt
+++ app/src/main/java/es/ua/eps/filmoteca/AboutActivity.kt
@@ -2,10 +2,19 @@ package es.ua.eps.filmoteca
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class AboutActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        }
+
+    fun toast(view: android.view.View) {
+        Toast.makeText(this, "Funcionalidad sin implementar",Toast.LENGTH_LONG).show()
     }
 }
