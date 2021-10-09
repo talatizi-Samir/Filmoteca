0c056cbbcffce02bf6758099e5865fe6a35ad3ac
Talatizi Samir <talatizisamir@gmail.com>
2021-10-09 13:49:59 +0200

    Practice2 Ejercicio 1. Intents implícitos

--- app/src/main/java/es/ua/eps/filmoteca/AboutActivity.kt
+++ app/src/main/java/es/ua/eps/filmoteca/AboutActivity.kt
@@ -1,10 +1,14 @@
 package es.ua.eps.filmoteca
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : AppCompatActivity() {
 
@@ -12,9 +16,41 @@ class AboutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
+
+          web_button.setOnClickListener{
+        val viewIntent = Intent(Intent.ACTION_VIEW,
+            Uri.parse("http://www.midominio.com"))
+
+        if (viewIntent.resolveActivity(packageManager) != null) {
+            startActivity(viewIntent) } }
+
+
+
+        soporte_button.setOnClickListener{
+            val viewIntent = Intent(Intent.ACTION_VIEW,
+                Uri.parse("mailto:midireccion@dominio.com"))
+
+            if (viewIntent.resolveActivity(packageManager) != null) {
+                startActivity(viewIntent) } }
+
+
+
+        volver_button.setOnClickListener {
+            finish()
         }
 
-    fun toast(view: android.view.View) {
-        Toast.makeText(this, "Funcionalidad sin implementar",Toast.LENGTH_LONG).show()
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
 }
